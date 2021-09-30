9764a5d7e69f63e3c1e1fe472d26a84fc987d41f
Yuya Matsushima <yuyam@fillin-inc.com>
2021-09-30 12:39:04 +0900

    Add link

---
+++ link.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+
+set -e
+
+CURRENT_DIR=`pwd`
+TARGETS=( \
+         ".gemrc" \
+         ".git" \
+         ".git-templates" \
+         ".gitconfig" \
+         "gitignore" \
+         ".tigrc" \
+         ".tmux.conf" \
+         ".vim" \
+         ".vimrc" \
+         ".gvimrc" \
+         ".zsh" \
+         ".zshrc" \
+         ".zshenv" \
+       )
+
+for TARGET in ${TARGETS[@]}
+do
+  if [ $TARGET = "gitignore" ]; then
+    ln -s $CURRENT_DIR/$TARGET $HOME/.$TARGET
+  else
+    ln -s $CURRENT_DIR/$TARGET $HOME/$TARGET
+  fi
+done
