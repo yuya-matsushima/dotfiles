80ae0f62902599b4df46841d7211c68501eab862
Yuya Matsushima <yuyam@fillin-inc.com>
2023-03-29 20:33:37 +0900

    fix homebrew problems for first install

--- bin/homebrew.sh
+++ bin/homebrew.sh
@@ -6,6 +6,11 @@ if ! command -v brew &> /dev/null
 then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [ -f /opt/homebrew/bin/brew ]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    else
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
 else
     echo "Homebrew is installed"
 fi
