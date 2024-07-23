5115781d0cdc5dee102982e8c2f5c8a758be3d32
Yuya Matsushima <yuyam@fillin-inc.com>
2024-07-23 09:47:15 +0900

    Add AltTab

--- bin/homebrew/main_machine.sh
+++ bin/homebrew/main_machine.sh
@@ -5,6 +5,7 @@ set -e
 brew install --cask \
           1password \
           1passsword-cli \
+          alt-tab \
           adobe-creative-cloud \
           appcleaner \
           chromedriver \
