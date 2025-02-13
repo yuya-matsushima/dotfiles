94ca7a1ec192afeb2fb4e37da7bdeed24ce44bcb
Yuya Matsushima <yuyam@fillin-inc.com>
2025-02-13 09:12:02 +0900

    remove alt-tab

--- bin/homebrew/main_machine.sh
+++ bin/homebrew/main_machine.sh
@@ -5,7 +5,6 @@ set -e
 brew install --cask \
           1password \
           1passsword-cli \
-          alt-tab \
           adobe-creative-cloud \
           appcleaner \
           chromedriver \
