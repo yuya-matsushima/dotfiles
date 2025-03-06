89c235fcdf0d78981e3e3b01e154aa58f2eb1e5d
Yuya Matsushima <yuyam@fillin-inc.com>
2025-03-07 07:22:50 +0900

    fix path

--- .zsh/asdf_completion.zsh
+++ .zsh/asdf_completion.zsh
@@ -1,7 +1,7 @@
 [[ $(which aws) == *.asdf/shims/aws* ]] && complete -C aws_completer aws
 [[ $(which terraform) == *.asdf/shims/terraform* ]] && complete -C terraform terraform
 if [[ $(which gcloud) == *.asdf/shims/gcloud* ]]; then
-  local gcloud_version=$(asdf current gcloud | awk '{ print $2 }')
+  local gcloud_version=$(asdf current gcloud | grep -v Version | awk '{ print $2 }')
   source $HOME/.asdf/installs/gcloud/${gcloud_version}/path.zsh.inc
   source $HOME/.asdf/installs/gcloud/${gcloud_version}/completion.zsh.inc
 fi
