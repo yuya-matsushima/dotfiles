5d422635c2973e853a55a72731072b6a41bfea5d
Yuya Matsushima <yuyam@fillin-inc.com>
2025-12-22 22:54:02 +0900

    update font.sh

--- bin/homebrew/font.sh
+++ bin/homebrew/font.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-brew tap homebrew/cask-fonts
-
 brew install --cask \
           font-biz-udgothic \
           font-biz-udmincho \
@@ -11,5 +9,8 @@ brew install --cask \
           font-biz-udpmincho \
           font-jetbrains-mono \
           font-jetbrains-mono-nerd-font \
+          font-moralerspace-hw \
+          font-moralerspace-jpdoc \
           font-noto-sans-cjk-jp \
-          font-noto-serif-cjk-jp
+          font-noto-serif-cjk-jp \
+          font-udev-gothic-nf
