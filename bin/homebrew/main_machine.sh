a248c65c199aa22fd796732f8622e1f29a290774
Yuya Matsushima <yuyam@fillin-inc.com>
2025-12-24 18:46:13 +0900

    remove adobe-creative-cloud from homebrew cask install list

--- bin/homebrew/main_machine.sh
+++ bin/homebrew/main_machine.sh
@@ -7,7 +7,6 @@ brew install switchaudio-osx
 brew install --cask \
           1password \
           1passsword-cli \
-          adobe-creative-cloud \
           alt-tab \
           appcleaner \
           aqua-voice \
